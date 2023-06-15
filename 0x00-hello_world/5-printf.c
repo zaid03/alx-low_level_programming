841e850792b1f7bb1be7123e0118b9c3e53aab25
zaid03 <terguyzaid@gmail.com>
2023-06-15 08:10:51 -0700

    task 5 try 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  * 
  * main - Entry point
  *
- * Return: always(0) success
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	Return (0);
+	return (0);
 }
