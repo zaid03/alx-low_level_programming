4f914db31086b2c07c72c462022b6b81635a84ea
zaid03 <terguyzaid@gmail.com>
2023-06-20 07:43:45 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/*
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_putchar\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+in this dir we will be learning more about nested loops and functions in C
