a40aa2189d17b4df1db0ba2ed034d76009e571e5
zaid03 <terguyzaid@gmail.com>
2023-06-20 09:12:24 -0700

    task 0 try 100

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-/*
- * main - entry point
- *
- * Return: always 0
+/**
+ * main - Entry point
+ * Return: Always 0
  */
 
 int main(void)
+
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,7 +16,8 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-        _putchar('r');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
+
 }
