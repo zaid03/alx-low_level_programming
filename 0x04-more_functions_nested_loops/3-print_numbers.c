0bb770cc3138ef96b43b46ae0e5a9c2cb802ebea
zaid03 <terguyzaid@gmail.com>
2023-06-22 03:28:53 -0700

    task 3 try 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print 0123456789
- * 
+ *
  * Return: void
  */
 
@@ -10,9 +10,9 @@ void print_numbers(void)
 {
 	char c;
 
-	for(c = '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-        _putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
