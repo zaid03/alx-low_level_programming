aa264a677b10be670ba27bf3d4ce2a712fc2fd5f
zaid03 <terguyzaid@gmail.com>
2023-06-22 04:01:13 -0700

    task 5 try 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,18 @@
  * Return: void
  */
 
-int more_numbers(void)
+void more_numbers(void)
 {
-	int i, j;
 
+int i, j;
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; i <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar('1');
-				_putchar(j % 10 + '0');
-			}
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
