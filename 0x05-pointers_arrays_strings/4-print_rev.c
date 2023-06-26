9c7746094c7ecab2019974719c24e51cc69e3306
zaid03 <terguyzaid@gmail.com>
2023-06-26 09:07:20 -0700

    task 4 try 2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,11 @@ void print_rev(char *s)
 {
 	int index;
 
-	for(index = 0; str[index] != '\0'; index ++)
+	for(index = 0; s[index] != '\0'; index ++)
 	{
 		;
 	}
-	for(index = index - 1; str[index] != '\0'; index --)
+	for(index = index - 1; s[index] != '\0'; index --)
 	{
 		putchar(s[index]);
 
