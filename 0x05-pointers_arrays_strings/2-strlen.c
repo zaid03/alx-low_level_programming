d7f156d7675b3d90f70abf233667bbd021224bb3
zaid03 <terguyzaid@gmail.com>
2023-06-26 08:33:14 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns lenth of string
+ *
+ * @s: input of the string
+ *
+ * Return: lennth of string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		;
+	}
+	return (index);
+}
