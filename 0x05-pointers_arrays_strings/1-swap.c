df17a9f60c79fa6e7e16ab0b13a2ef16497677f7
zaid03 <terguyzaid@gmail.com>
2023-06-26 08:23:40 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of a and b
+ *
+ * @a: first entry point
+ * @b: second entry point
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int pepe;
+
+	pepe = *a;
+	*a = *b;
+	*b = c;
+}
