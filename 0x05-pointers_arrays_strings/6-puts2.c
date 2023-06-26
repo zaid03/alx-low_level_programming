62172b67a8fc1b10c2cb93a689ad2a7b90dabec1
zaid03 <terguyzaid@gmail.com>
2023-06-26 09:25:57 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 
 /**
  * puts2 - Write a function that prints every other character of a string, 
- *         starting with the first character, followed by a new line.
+ * starting with the first character, followed by a new line
  *
  * @s: This pointer point to a char or string
  *
