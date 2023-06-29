c2f3e39f75bb043d4568c72047bd79b546b73bb1
zaid03 <terguyzaid@gmail.com>
2023-06-29 15:51:31 -0700

    task  3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: char
+ * @s2:char
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int comp;
+
+	while (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+
+	}
+	comp = *s1 - *s2;
+	return (com);
+}
