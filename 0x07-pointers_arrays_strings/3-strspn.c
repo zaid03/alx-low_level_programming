cfb3fc607a12c585a48ff4dc68c38bc5e17613cf
zaid03 <terguyzaid@gmail.com>
2023-07-03 18:37:45 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strpn - get the lenght of a prefix substring
+ *
+ * @accept: pointer to get the bytes
+ * @s: pointer to string char type
+ *
+ * Return: number of bytes in s which are from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a;
+	unsigned int b;
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				break;
+			}
+			if (!accept[b])
+			{
+				break;
+			}
+		}
+		return (a);
+	}
+}
