b86768089584634343053477a122e429e31dc53f
zaid03 <terguyzaid@gmail.com>
2023-07-17 09:10:21 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Free the memory of the structure.
+ * @d: Structure.
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+	}
+	free(d);
+}
